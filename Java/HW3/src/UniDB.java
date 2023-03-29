15ddd692d454829d531a459e8af2e5e13ad58b4f
Rishik Sarkar <rs1894@scarletmail.rutgers.edu>
2023-03-29 04:27:36 -0400

    has-taken regen, Java files added

---
+++ Java/HW3/src/UniDB.java
@@ -0,0 +1,209 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class UniDB {
+
+    private static String degreeName(String dname) {
+        if (dname.equalsIgnoreCase("Eng")) {
+            return "BA in " + dname;
+        }
+
+        return "BS in " + dname;
+    }
+
+    private static void formatStudent(int sid) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/hw3", "root", "Neongourami123!");
+
+            Statement stmt = con.createStatement();
+
+            ResultSet sn = stmt.executeQuery("SELECT last_name, first_name FROM Students WHERE id=" + sid);
+            while (sn.next()) {
+                System.out.println(sn.getString(1) + ", " + sn.getString(2));
+            }
+
+            System.out.println("ID: " + sid);
+
+            ResultSet maj = stmt.executeQuery("SELECT dname FROM Majors WHERE sid=" + sid);
+            if (maj.isLast()) {
+                System.out.println("Major: " + degreeName(maj.getString(1)));
+            } else {
+                String major = "";
+                major += "Majors: ";
+                while (maj.next()) {
+                    major += degreeName(maj.getString(1));
+                    if (!maj.isLast()) {
+                        major += ", ";
+                    }
+                }
+                System.out.println(major);
+            }
+
+            ResultSet min = stmt.executeQuery("SELECT dname FROM Minors WHERE sid=" + sid);
+            if (min.isLast()) {
+                System.out.println("Minor: " + min.getString(1));
+            } else {
+                String minor = "";
+                minor += "Minors: ";
+                while (min.next()) {
+                    minor += min.getString(1);
+                    if (!min.isLast()) {
+                        minor += ", ";
+                    }
+                }
+                System.out.println(minor);
+            }
+
+            System.out.printf("GPA: %.3f \n", getGPA(sid));
+            System.out.println("Credits: " + getCredits(sid));
+            System.out.println();
+
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    private static int gradePoints(String grade) {
+        grade = grade.toLowerCase();
+
+        switch (grade) {
+            case "a":
+                return 4;
+            case "b":
+                return 3;
+            case "c":
+                return 2;
+            case "d":
+                return 1;
+            case "f":
+                return 0;
+            default:
+                return 0;
+        }
+    }
+
+    // select distinct grade, count(grade) from hastaken group by grade;
+    // select count(grade) from hastaken;
+
+    private static double getGPA(int sid) {
+        double gpa = 0;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/hw3", "root", "Neongourami123!");
+
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(
+                    "SELECT H.grade, C.credits FROM HasTaken H LEFT JOIN Classes C ON H.name=C.name WHERE H.sid="
+                            + sid);
+
+            double gradePointSum = 0;
+            double totalCredits = 0;
+            while (rs.next()) {
+                gradePointSum += gradePoints(rs.getString(1)) * rs.getInt(2);
+                totalCredits += rs.getInt(2);
+            }
+            gpa = gradePointSum / totalCredits * 1.0;
+
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return gpa;
+    }
+
+    private static int getCredits(int sid) {
+        int totalCredits = 0;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/hw3", "root", "Neongourami123!");
+
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(
+                    "SELECT C.credits FROM HasTaken H LEFT JOIN Classes C ON H.name=C.name WHERE H.sid="
+                            + sid + " AND H.grade != 'F'");
+
+            while (rs.next()) {
+                totalCredits += rs.getInt(1);
+            }
+
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return totalCredits;
+    }
+
+    // Query 1
+    private static void searchName() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/hw3", "root", "Neongourami123!");
+
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from students");
+            while (rs.next()) {
+                formatStudent(rs.getInt(3));
+            }
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @SuppressWarnings("resource")
+    private static int runQuery() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Which query would you like to run (1-8)?");
+        int queryNum = scan.nextInt();
+        scan.nextLine();
+
+        switch (queryNum) {
+            case 1:
+                searchName();
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                System.out.println("Goodbye.");
+                break;
+            default:
+                System.out.println("Please enter a valid query (1-8)!");
+                return runQuery();
+        }
+
+        return queryNum;
+    }
+
+    public static void main(String args[]) {
+        System.out.println("Welcome to the university database. Queries available:");
+        System.out.println("1. Search students by name.");
+        System.out.println("2. Search students by year.");
+        System.out.println("3. Search for students with a GPA >= threshold.");
+        System.out.println("4. Search for students with a GPA <= threshold.");
+        System.out.println("5. Get department statistics.");
+        System.out.println("6. Get class statistics.");
+        System.out.println("7. Execute an arbitrary SQL query.");
+        System.out.println("8. Exit the application.");
+
+        int qnum = 0;
+
+        while (qnum != 8) {
+            qnum = runQuery();
+        }
+    }
+}
